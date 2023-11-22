65646b384345fe682560bde8736a401fb678b471
InglisSofia <>
2023-11-22 17:47:46 +0000

    Create value errors and get new question or chance to make a valid input in game

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 import random
 
-#the game with loop function
-while True:
+#the game, with loop function
+while True:  
     player_move = input("Make your move (rock, paper, scissors):")
     moves = ["rock", "paper", "scissors"]
-    computer_move = random.choice(moves)
+    if player_move in moves:
+        computer_move = random.choice(moves)
+    elif [""]: 
+        print("Make a valid choice!")
+        continue       
+    
     print(f"\nYour choice {player_move}, computers choice {computer_move}.\n")
-#determines winner
+            
+    #outcomes to determine winner  
     if player_move == computer_move:
         print(f"It's a tie! Both chose {player_move}")
     elif player_move == "rock":
@@ -16,22 +22,24 @@ while True:
             print("Paper beats rock! You lose...")
     elif player_move == "scissors":
         if computer_move == "paper":
-            print("Scissors beat paper! You win!")
+            print("Scissors beats paper! You win!")
         else:
             print("Rock beats scissors! You lose...")
     elif player_move == "paper":
         if computer_move == "rock":
             print("Paper beats rock! You win!")
         else:
-            print("Scissors beat paper! You lose...")
-#question for player to continue och quit game
+            print("Scissors beats paper! You lose...")
+
+    
+
+    #question for player to continue och quit game
     do_over = input("One more time? (yes/no):")
+    
     if do_over.lower() == "no":
         break
     if do_over.lower() == "yes":
         continue
     else:
         print("Make a valid choice! (yes/no)")
-        break
-
-
+        do_over = input("One more time? (yes/no):")
