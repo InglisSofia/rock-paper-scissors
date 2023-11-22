b2d0857d3b81d94b063f9ea3eec56c66d53a1a71
InglisSofia <>
2023-11-22 17:39:47 +0000

    Add while loop and question to continue the game

--- run.py
+++ run.py
@@ -1,26 +1,31 @@
 import random
 
-#the game moves
-player_move = input("Make your move (rock, paper, scissors):")
-moves = ["rock", "paper", "scissors"]
-computer_move = random.choice(moves)
-print(f"\nYour choice {player_move}, computers choice {computer_move}.\n")
+#the game with loop function
+while True:
+    player_move = input("Make your move (rock, paper, scissors):")
+    moves = ["rock", "paper", "scissors"]
+    computer_move = random.choice(moves)
+    print(f"\nYour choice {player_move}, computers choice {computer_move}.\n")
+#determines winner
+    if player_move == computer_move:
+        print(f"It's a tie! Both chose {player_move}")
+    elif player_move == "rock":
+        if computer_move == "scissors":
+            print("Rock beats scissors! You win!")
+        else:
+            print("Paper beats rock! You lose...")
+    elif player_move == "scissors":
+        if computer_move == "paper":
+            print("Scissors beat paper! You win!")
+        else:
+            print("Rock beats scissors! You lose...")
+    elif player_move == "paper":
+        if computer_move == "rock":
+            print("Paper beats rock! You win!")
+        else:
+            print("Scissors beat paper! You lose...")
+#question for player to continue or quit the game
+    do_over = input("One more time? (yes/no):")
+    if do_over.lower() == "no":
+        break
 
-#outcomes to determine winner
-if player_move == computer_move:
-    print(f"It's a tie! Both chose {player_move}")
-elif player_move == "rock":
-    if computer_move == "scissors":
-        print("Rock beats scissors! You win!")
-    else:
-        print("Paper beats rock! You lose...")
-elif player_move == "scissors":
-    if computer_move == "paper":
-        print("Scissors beat paper! You win!")
-    else:
-        print("Rock beats scissors! You lose...")
-elif player_move == "paper":
-    if computer_move == "rock":
-        print("Paper beats rock! You win!")
-    else:
-        print("Scissors beat paper! You lose...")
